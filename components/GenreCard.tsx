c10c97509eddee9af32fc8b76e1b92e2ece11eea
Alexander D. Cardosi <13227184+adcar@users.noreply.github.com>
2022-05-13 21:34:32 -0400

    Abstracted into a new function and got blur working

---
+++ components/GenreCard.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function GenreCard({genre}: Props) {
+  const [isVisible, setVisible] = useState(false);
+  return (
+    <Link href={`/movies?genre=${genre.id}`}>
+    <a>
+      <div
+        className="relative rounded-md"
+        onMouseOver={() => {
+          setVisible(true);
+        }}
+        onMouseLeave={() => setVisible(false)}
+      >
+        <Image
+          width={300}
+          height={169}
+          layout="responsive"
+          src={`/thumbnails/${genre.name.toLowerCase()}.jpg`}
+          className="absolute top-0 left-0 h-full w-full rounded-md"
+        />
+        <div
+          className="absolute top-0 left-0 h-full w-full rounded-md backdrop-blur-lg transition-all duration-300 ease-in-out"
+          style={{
+            opacity: isVisible ? 1 : 0,
+          }}
+        />
+        <div className="absolute top-0 left-0 flex h-full w-full items-center justify-center rounded-md bg-gradient-to-b from-slate-800/60 via-slate-800/80 to-slate-800/60 shadow-md transition-all duration-300 ease-in-out hover:shadow-xl">
+          <h1 className="text-2xl font-bold">{genre.name}</h1>
+        </div>
+      </div>
+    </a>
+  </Link>
+  )
+}
+
+interface Props {
+  genre: {name: string; id: number}
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,9 +4,10 @@ import Head from "next/head";
 import Link from "next/link";
 import genres from "../utils/genres";
 import { useState } from "react";
+import GenreCard from "../components/GenreCard";
 
 const Dashboard: NextPage = () => {
-  const [isVisible, setVisible] = useState(false);
+
   return (
     <>
       <Head>
@@ -22,34 +23,7 @@ const Dashboard: NextPage = () => {
       <div>
         <div className="container mx-auto grid grid-cols-2 place-content-center gap-10 px-5 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {genres.map((genre, i) => (
-            <Link href={`/movies?genre=${genre.id}`} key={i}>
-              <a>
-                <div
-                  className="relative rounded-md"
-                  onMouseOver={() => {
-                    setVisible(true);
-                  }}
-                  onMouseLeave={() => setVisible(false)}
-                >
-                  <Image
-                    width={300}
-                    height={169}
-                    layout="responsive"
-                    src={`/thumbnails/${genre.name.toLowerCase()}.jpg`}
-                    className="absolute top-0 left-0 h-full w-full rounded-md"
-                  />
-                  <div
-                    className="absolute top-0 left-0 h-full w-full rounded-md backdrop-blur-lg transition-all duration-300 ease-in-out"
-                    style={{
-                      opacity: isVisible ? 1 : 0,
-                    }}
-                  />
-                  <div className="absolute top-0 left-0 flex h-full w-full items-center justify-center rounded-md bg-gradient-to-b from-slate-800/60 via-slate-800/80 to-slate-800/60 shadow-md transition-all duration-300 ease-in-out hover:shadow-xl">
-                    <h1 className="text-2xl font-bold">{genre.name}</h1>
-                  </div>
-                </div>
-              </a>
-            </Link>
+              <GenreCard key={genre.id} genre={genre} />
           ))}
         </div>
       </div>
